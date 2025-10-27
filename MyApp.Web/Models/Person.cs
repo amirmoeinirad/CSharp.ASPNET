56a669d23c4a77eab7896aa8678e04ca53e87dcc
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-10-27 17:06:22 +0330

    Edit EF Core implementation

---
+++ MyApp.Web/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using NodaTime;
+
+namespace MyApp.Web.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public Instant CreatedAt { get; set; }   // NodaTime Instant
+        public Instant? UpdatedAt { get; set; }
+    }
+}
