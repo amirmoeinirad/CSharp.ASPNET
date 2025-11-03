43ddb696de8f49dde46b8a153f0ad93262ee37cb
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-11-03 16:35:09 +0330

    Review

--- MyApp.Application/Interfaces/IPersonRepository.cs
+++ MyApp.Application/Interfaces/IPersonRepository.cs
@@ -5,22 +5,29 @@ namespace MyApp.Application.Interfaces
     public interface IPersonRepository
     {
         // CRUD operations for Person entity
+        // Here, we define the business logic methods for managing Person entities.
+        // The actual implementation will be in the Infrastructure layer.
 
         // 'Task' is used for asynchronous programming.
 
-        // Get a person by ID
+        // The 'Person' class represents a person entity in the domain model.
+        // It is mapped to a database table.
+
+
+        // (1) Get a person by ID from the database asynchronously.
         Task<Person?> Get(int id);
 
-        // Get all people in the database
+        // (2) Get all people from the database asynchronously.
+        // The method returns a list of Person objects asynchronously.
         Task<List<Person>> GetAllPeople();
 
-        // Add a new person to the database
+        // (3) Add a new person to the database asynchronously.
         Task Add(Person p);
 
-        // Update an existing person in the database
+        // (4) Update an existing person in the database asynchronously.
         Task Update(Person p);
 
-        // Delete a person from the database by ID
+        // (5) Delete a person from the database by ID asynchronously.
         Task Delete(int id);
     }
 }
